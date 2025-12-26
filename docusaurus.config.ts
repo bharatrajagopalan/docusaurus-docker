d1480c098a49fc4a7f15d0150880a8ec0b3f8459
Bharat Rajagopalan <bharat_rajagopalan@uk.ibm.com>
2025-12-27 00:54:10 +0100

    fixed baseUrl to docusaurus-docker

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   url: 'https://bharatrajagopalan.github.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-docker/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
